6dfea80dfb2a608ca533841ea6ccb8d692237af7
BellsForHer <arhall88@gmail.com>
2023-05-04 19:57:06 -0500

    created employee model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_11_22_185403) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_002717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "slug"
@@ -51,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2021_11_22_185403) do
     t.datetime "invitation_expiration", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
