fd456b6eb061a185b827c1758b185f6683225bc8
BellsForHer <arhall88@gmail.com>
2023-05-08 18:47:30 -0500

    Get Team and all teams works

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -14,6 +14,40 @@ module Api
                   end
             end
 
+            def index
+                teams = Team.all
+                payload = {
+                    teams: TeamBlueprint.render_as_hash(teams),
+                    status: 200
+                }
+                render_success(payload: payload)
+            end
+            
+            def show
+                team = Team.find(params[:id])
+                payload = {
+                    team: TeamBlueprint.render_as_hash(team),
+                    status: 200
+                }
+                render_success(payload: payload)
+            end
+            
+            def update
+                result = Teams::Operations.update_team(params)
+                render_error(errors: result.errors.all, status: 400) and return unless result.success?
+                payload = {
+                    team: TeamBlueprint.render_as_hash(result.payload),
+                    status: 201
+                }
+                render_success(payload: payload)
+            end
+
+            def destroy
+                team = Team.find(params[:id])
+                team.destroy
+                render_success(payload: "Team has been deleted!", status: 200)
+            end
+
         end
     end
 end
--- app/services/teams/operations.rb
+++ app/services/teams/operations.rb
@@ -5,10 +5,10 @@ module Teams
             return ServiceContract.success(team) if team.save
             ServiceContract.error(team.errors.full_messages)
         end
-        def self.update_book(params)
-            book = Book.find(params[:id])
-            return ServiceContract.success(book) if book.update(title: params[:title], summary: params[:summary], image_path: params[:image_path], category_ids: params[:category_ids], author_ids: params[:author_ids])
-            ServiceContract.error(book.errors.full_messages)
+        def self.update_team(params)
+            team = Team.find(params[:id])
+            return ServiceContract.success(team) if team.update(name: params[:name], description: params[:description], user_id: params[:user_id])
+            ServiceContract.error(team.errors.full_messages)
         end
     end
 end
