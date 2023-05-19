888b534fcf50141137c77e9a957f010addbba247
Savon Williams <44207109+Savon817@users.noreply.github.com>
2023-05-18 20:14:28 -0500

    Update: more data for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,32 @@ Job.create(job_titles: "Director, Software Engineering")
 Job.create(job_titles: "Business Analyst")
 Job.create(job_titles: "Product Owner")
 Job.create(job_titles: "UX Designer")
+
+# defining teams
+teams = [
+    {
+      name: 'Team A',
+      description: 'Description of Team A',
+      employees: [
+        { first_name: 'John', last_name: 'Doe' },
+        { first_name: 'Jane', last_name: 'Smith' },
+        { first_name: 'Mike', last_name: 'Johnson' }
+      ]
+    },
+    {
+      name: 'Team B',
+      description: 'Description of Team B',
+      employees: [
+        { first_name: 'Alice', last_name: 'Williams' },
+        { first_name: 'Bob', last_name: 'Brown' },
+        { first_name: 'Johnny', last_name: 'Cash' }
+      ]
+    }
+  ]
+  # creating teams and their employees
+  teams.each do |team_data|
+    team = Team.create(name: team_data[:name], description: team_data[:description])
+    team_data[:employees].each do |employee_data|
+      team.employees.create(first_name: employee_data[:first_name], last_name: employee_data[:last_name])
+    end
+  end
